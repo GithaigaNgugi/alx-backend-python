8b895041f5e118f1ffc6d0286b5890318f38e8f9
GithaigaNgugi <ngugibrian00@gmail.com>
2025-07-21 10:08:58 +0100

    create user stream

---
+++ python-generators-0x00/0-stream_users.py
@@ -0,0 +1,29 @@
+import mysql.connector
+
+def stream_users():
+    """
+    Generator function to stream rows from the user_data table one by one.
+    Yields:
+        dict: A dictionary with user_id, name, email, and age.
+    """
+    try:
+        connection = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            password="",  # Update with your actual password if set
+            database="ALX_prodev"
+        )
+
+        cursor = connection.cursor(dictionary=True)
+        cursor.execute("SELECT * FROM user_data")
+
+        for row in cursor:
+            yield row
+
+    except mysql.connector.Error as err:
+        print(f"Error: {err}")
+    finally:
+        if cursor:
+            cursor.close()
+        if connection:
+            connection.close()
--- python-generators-0x00/README.md
+++ python-generators-0x00/README.md
@@ -1,2 +1,1 @@
-
-├── README.md                # Project documentation
+README.md                # Project documentation
